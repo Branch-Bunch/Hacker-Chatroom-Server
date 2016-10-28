3ae4b163245a7f82591a44b4e6edce3d85522e9a
kirinrastogi <rastogikirin@gmail.com>
2016-10-28 14:05:33 -0400

    Add server.js and ecstatic

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const ecstatic = require('ecstatic');
+const server = require('http').createServer(
+  ecstatic({ root: __dirname, handleError: false });
+);
+const p2pserver = require('socket.io-p2p-server').Server
+const io = require('socket.io')(server);
+
+server.listen(3030, () =>{
+  console.log('Listening on 3030');
+});
+
+io.use(p2pserver);
+
+io.on('connection', (socket) =>{
+  socket.on('peer-msg', (data) =>{
+    console.log('Message from peer: %s', data);
+    socket.broadcast.emit('peer-msg', data);
+  });
+
+  socket.on('peer-file', (data) =>{
+    console.log('File from peer: %s', data);
+    socket.broadcast.emit('peer-file', data);
+  });
+
+  socket.on('go-private', (data) =>{
+    socket.broadcast.emit('go-private', data);
+  });
+});
