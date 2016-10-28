09c35bbb4d1477167c9e065d33a8153eaec76cc4
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-28 19:08:20 -0400

    Process env for port

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = require('express')()
 const server = require('http').createServer()
 const io = require('socket.io')(server)
 
-server.listen(3030, () => {
+server.listen(process.env.PORT || 3030, () => {
   console.log('Listening on 3030')
 })
 
 io.on('connection', (socket) => {
-    socket.emit('test', 'client connected')
-    //console.log('CONNECTED', socket)
+    socket.emit('test', 'Connected to web socket')
+    console.log('Client Connected')
 
 	socket.on('general', (msg) => {
 		io.emit('general', msg)
@@ -27,5 +27,4 @@ io.on('connection', (socket) => {
   	socket.on('create', function(room) {
     	socket.join(room);
   	});
-
 })
