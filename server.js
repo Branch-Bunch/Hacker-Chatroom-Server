b6f3b2c95c20c4a305989897ffba79f48a1a24d3
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-28 14:10:56 -0400

    Added client

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ const server = require('http').createServer(
 const p2pserver = require('socket.io-p2p-server').Server
 const io = require('socket.io')(server);
 
-server.listen(3030, () =>{
+server.listen(3030, () => {
   console.log('Listening on 3030');
 });
 
 io.use(p2pserver);
 
-io.on('connection', (socket) =>{
-  socket.on('peer-msg', (data) =>{
+io.on('connection', (socket) => {
+  socket.on('peer-msg', (data) => {
     console.log('Message from peer: %s', data);
     socket.broadcast.emit('peer-msg', data);
   });
 
-  socket.on('peer-file', (data) =>{
+  socket.on('peer-file', (data) => {
     console.log('File from peer: %s', data);
     socket.broadcast.emit('peer-file', data);
   });
 
-  socket.on('go-private', (data) =>{
+  socket.on('go-private', (data) => {
     socket.broadcast.emit('go-private', data);
   });
 });
