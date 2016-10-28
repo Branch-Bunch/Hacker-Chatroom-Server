be7567ed60d8fd0e9a8071afdb7b6a0e62c58f04
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-28 18:18:15 -0400

    Added basic web socket server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-'use strict';
+'use strict'
 
-const app = require('express')();
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const app = require('express')()
+const server = require('http').createServer()
+const io = require('socket.io')(server)
 
 server.listen(3030, () => {
-  console.log('Listening on 3030');
-});
+  console.log('Listening on 3030')
+})
 
+io.on('connection', (socket) => {
+    socket.emit('test', 'Are you reading this?')
+    console.log('CONNECTED', socket)
+})
