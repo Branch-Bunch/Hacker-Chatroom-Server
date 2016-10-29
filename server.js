f1e9efe3983d8df2e7754126aaa47bcb5dfc4c42
Jacky Chiu <jacky.chiu888@gmail.com>
2016-10-28 20:00:16 -0400

    port varible

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-server.listen(process.env.PORT || 3030, () => {
-    console.log('Listening on 3030')
+const port = process.env.PORT || 3030
+server.listen(port, () => {
+    console.log('Listening on ', port)
 })
 
 app.get('/rooms', (req, res) => {
@@ -16,19 +17,19 @@ io.on('connection', (socket) => {
     socket.emit('test', 'Connected to web socket')
     console.log('Client Connected')
 
-	socket.on('general', (msg) => {
-		io.emit('general', msg)
-	})
+    socket.on('general', (msg) => {
+        io.emit('general', msg)
+    })
 
-	socket.on('private', (sender, msg) => {
-		// private message received
-	})
+    socket.on('private', (sender, msg) => {
+        // private message received
+    })
 
-	socket.on('disconnect', () => {
-		io.emit('client disconnected')
-	})
+    socket.on('disconnect', () => {
+        io.emit('client disconnected')
+    })
 
-  	socket.on('create', (room) => {
-                socket.join(room)
-  	})
+    socket.on('create', (room) => {
+        socket.join(room)
+    })
 })
