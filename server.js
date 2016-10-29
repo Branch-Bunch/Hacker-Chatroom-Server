53c77a85a4839cf6c1a37d9229afb48b5927c4d3
BenEmdon <benjaminemdon@gmail.com>
2016-10-28 22:17:44 -0400

    Seperated out room logic for now

--- server.js
+++ server.js
@@ -3,15 +3,14 @@
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const rooms = require('./rooms.js')
 
 const port = process.env.PORT || 3030
 server.listen(port, () => {
     console.log('Listening on ', port)
 })
 
-app.get('/rooms', (req, res) => {
-    res.send(io.sockets.adapter.rooms)
-})
+app.get('/rooms', rooms.getRooms)
 
 io.on('connection', (socket) => {
     socket.emit('test', 'Connected to web socket')
